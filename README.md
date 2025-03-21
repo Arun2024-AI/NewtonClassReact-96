d25ba78b2fdfb567a3a22a78c69676b8ef1fb991
Arun2024-AI <atulgiri649@gmail.com>
2025-03-21 19:32:43 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# NewtonClassReact-96
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/Arun2024-AI/NewtonClassReact-96)
---
+++ src/App.js
@@ -0,0 +1,97 @@
+import React from 'react';
+import './style.css';
+
+// export default function App() {
+//   const text = "NSTians"
+//   return (
+//     <div>
+//       <h1>Hello {text}</h1>
+//       <p>Start editing to see some magic happen :)</p>
+//     </div>
+//   );
+// }
+
+// export default function App() {
+//   const isloggedin = "NSTians"
+//   return (
+//     <div>
+//       <div>{isloggedin ? <h1>Welcome</h1> :
+//       <h1>Please sign in</h1>}</div>
+//     </div>
+//   );
+// }
+
+// export default function App() {
+//   const isloggedin = false;
+//   return (
+//     <div>
+//       <div>{isloggedin ? <h1>Welcome</h1> :
+//       <h1>Please sign in</h1>}</div>
+//     </div>
+//   );
+// }
+
+// export default function App() {
+//   const isloggedin = false;
+//   if(isloggedin){
+//     return <h1>Welcome</h1>
+//   } else{
+//     return <h1>Please sign in</h1>
+//   }
+
+// }
+
+// export default function App() {
+//   const weatherCondition = "Rainy"
+//   switch(weatherCondition){
+//     case("sunny"): {
+//       return <h1>Sunny</h1>
+//     }
+//     case("Rainy"): {
+//       return <h1>Rainy</h1>
+//     }
+//     default: {
+//       return <h1>All</h1>
+//     }
+//   }
+// }
+
+// export default function App() {
+//   const isloggedin = true;
+//   return <div>{isloggedin && <h1>Welcome</h1>}</div>
+// }
+
+// export default function App() {
+//     const user = {
+//       name: "Arun"
+//     };
+
+//   return <h1>{user.name || 'anonymous user'}</h1>
+// }
+
+// export default function App() {
+//   function fetchUser() {
+//     return {
+//       name: 'Arun'
+//     };
+//   }
+
+//   const user = fetchUser();
+
+//   return <h1>{user.name}</h1>;
+// }
+
+// export default function App(){
+//   const students = ['s1','s2','s3']
+//   return <div>{
+//     [<h1>s1</h1>,<h1>s3</h1>]}</div>
+// }
+
+export default function App() {
+  const students = ['s1', 's2', 's3'];
+  const studentselements = [];
+  for (let i = 0; i < students.length; i++) {
+    studentselements.push(<h1>{students[i]}</h1>);
+  }
+  return <div>{studentselements}</div>;
+}
