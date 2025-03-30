3f92497a7cea08288ec490bf708844e5f2fae780
Arun2024-AI <atulgiri649@gmail.com>
2025-03-30 22:07:16 +0530

    New lIne Practice time

--- src/App.js
+++ src/App.js
@@ -395,20 +395,20 @@ export default function App() {
 
 
 
-export default function App() {
-  const [meassage,setMessage] =useState("Hello")
+// export default function App() {
+//   const [meassage,setMessage] =useState("Hello")
 
-  const handleMessage =()=>{
-    setMessage("buttton was click")
+//   const handleMessage =()=>{
+//     setMessage("buttton was click")
 
-  }
-  return (
-    <div>
-      <h1>{meassage}</h1>
-      <button onClick={handleMessage}></button>
-    </div>
-  )
-}
+//   }
+//   return (
+//     <div>
+//       <h1>{meassage}</h1>
+//       <button onClick={handleMessage}></button>
+//     </div>
+//   )
+// }
 
 
 
